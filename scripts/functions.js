7f9e4a30c431c3835a0eae78bd266b5982f3b3c6
vincentmachariakimani <vincentmachariakimani@gmail.com>
2025-03-12 11:44:50 +0300

    scope

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,158 +1,35 @@
 /**
- * Functions
- * DRY ---> Do Not Repeat Yourself
+ * Scope -> Mostly comes into  play when dealing with variables
+ * Global scope & Local scope
  *
- * To avoid repeating  the same code through out our code base
- *   we use a funtion to wrap that code  and reuse it.
- *
- * All the rules used when creating varriables should be applied
- * Creating Function names
- * Every function in js always returns undefined unless we explicitely
- *  specify a return value using the return key word
  */
 
-function functionName(parameters) {
-  // fundtion body
-}
+let firstName = "James";
 
-// Function without parameters
-function morning() {
-  console.log("Good Morning Kimani, It's on Tuesday");
-}
-morning();
-// function with one parameter
-function square(a) {
-  // Return allows a function to give an output
-  // We can only have one return keyword unless we are uding selection statements
-  // the return kerword stops execution of a function
-  return a ** 2;
-}
-console.log(square(4));
-const output = square(3);
-console.log(output);
-console.log(typeof square);
+// In this function, a local scope is created where we are able
+//  to even reuse varriable names that are already in use in the
+//   global scope
+function lunch() {
+  // We are dealing with parameters, js auto assigns them the let keyword
+  // which means we can reassign but not redeclare the same variables inside the function
 
-// function with multiple parameters
-function sum(a = 0, b = 0) {
-  // Validation
-  // console.log(typeof b);
-  // console.log(typeof a);
-  if (typeof a === "number" && typeof b === "number") {
-    return a + b;
-  } else {
-    return "A number must be Provided for both a and b";
+  // const firstName = "Kimani";
+  console.log(firstName);
+  firstName = "kimani";
+  if (true) {
+    // var doesn't respect the scope principle which means 
+    // it exposes local varriables to the outer scope
+    let lastName = "Macharia";
+    console.log(lastName);
   }
+  // Variables inside the local scope are usually prioritized before
+  //    global scope
+  // We can be able to access global varriables inside local scopes
+  console.log(`${firstName} is prepairing some Lunch`);
 }
-//The order in which arguments are passed should match the parameters
-const num1 = 7;
-const num2 = 5;
-console.log(sum(num1, num2));
-console.log(sum(5, 2));
-console.log(sum(-1, -3));
+console.log(firstName);
 
-function greeting(firstName, lastName) {
-  return `Good morning ${firstName}, ${lastName} Have a nice day`;
-}
-console.log(greeting("Vincent", "Kimani"));
+lunch();
 
-// Es6 allows us to write shorter functions using the arrow function
+console.log(firstName);
 
-/**
- * functions in js are firstt class citizen meaning they
- *    can be treated like othe values
- *This is brought about by arrow functions & call back fuctions
-
- */
-
-const ongeza = sum;
-console.log(ongeza(10, 19));
-
-const add = (a, b) => a + b;
-console.log(add(8, 10));
-
-const ongezab = (a, b) => {
-  return a + b;
-};
-console.log(ongezab(14, 16));
-
-const greet = function (name) {
-  return `Good Evenning ${name}`;
-};
-
-console.log(greet("Kimani"));
-
-const weatherToday = "Rainy";
-
-function exerciseDog(dogName, dogBreed) {
-  if (weatherToday === "Rainy") {
-    return `${dogName} did not exercise due to rain`;
-  }
-  console.log(`Wake ${dogName} the ${dogBreed}`);
-  console.log(`Leash ${dogName} the ${dogBreed}`);
-  console.log(`Walk to the park ${dogName} the ${dogBreed}`);
-  console.log(`Throw the frisbee for ${dogName} the ${dogBreed}`);
-  console.log(`Walk home with ${dogName} the ${dogBreed}`);
-  console.log(`Unleash ${dogName} the ${dogBreed}`);
-  return `${dogName} is happy and tired!`;
-}
-
-const result = exerciseDog("Byron", "poodle");
-console.log(result);
-
-function functionName(ListOfParameters) {
-  console.log(ListOfParameters);
-  console.log("This is my first function");
-  return 3 + 3;
-}
-functionName(false);
-
-const outPut = functionName();
-
-console.log(outPut);
-
-function sum(a, b) {
-  console.log(`a = ${a}`);
-  console.log(`b = ${b}`);
-  return a + b;
-}
-console.log(sum(6, 9));
-console.log(sum(15, 88));
-console.log(sum(77, 54));
-console.log(sum(78, 100));
-
-function perimeterOfRectangle(l, w) {
-  console.log(`l = ${l}`);
-  console.log(`w= ${w}`);
-  return `Perimeter = ${(2*l) + (2* w)}`;
-}
-console.log(perimeterOfRectangle(7, 5));
-
-// functions are firstclass citizen meaning we can treat them like any other values
-// since It's still a function the rules of functions still apply
-// Now this becomes a function expresiion(function assigned to varriables)
-// The function expression can work without function names i.e anonymous functions
-
-// function expresiions don;\'t allow hoisting since the function is stored 
-// inside a varriable
-
-const perimeter = (l, w) => {
-  return l * 2 + w * 2;
-}
-
-console.log(perimeter(4, 6));
-
-const newVarriable = 9
-console.log(newVarriable);
-
-function calculator(z, a, c) {
-  // console.log(c(7, 5));
-  return c(z, a);
-  // console.log(x);
-  // At this point we are executing the function
-  //  perimeter through the placeholder
-  return z;
-}
-
-const z = 8;
-const outPut1 = calculator(7, 5, perimeter);
-console.log(outPut1);
