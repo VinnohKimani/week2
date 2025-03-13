87a1c15b47cd76eb9850ec7d9709a3370e772e97
vincentmachariakimani <vincentmachariakimani@gmail.com>
2025-03-13 10:32:53 +0300

    array methods

--- scripts/arrays.js
+++ scripts/arrays.js
@@ -22,12 +22,14 @@ console.log(students);
 
 console.log(students[4][2][`colorName`]);
 students[4] = "Nelson";
+students.splice(2, 0, "Bildad", "Denise", "George");
+// students.splice(2, 1);
 
 // Arrray methods
 // --->Mutating array methods
 //----(Push, Shift, Pop, Unshift)----
 
-const colors = ["Red"];
+const colors = ["Red", "Green", "Blue"];
 /**
  * Adding elements
  */
@@ -54,3 +56,45 @@ colors.shift();
 console.log(colors);
 const deletedColor2 = colors.shift();
 console.log(deletedColor2);
+
+/**
+ * Slice and splice
+ */
+// Slice --non-destructive method (Does not mutate the original array)
+// Allows us to make a copy of an array
+// Delete elements in an array
+
+const copyOfColors = colors.slice();
+console.log(copyOfColors);
+// Spread Operator(...)-> Makes a copy too
+const secondCopyOfColors = [...copyOfColors];
+
+const deletedColor3 = colors.slice(0);
+console.log(deletedColor);
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const deletedNumbers = numbers.slice(-5, -2);
+console.log(deletedNumbers);
+console.log(numbers);
+
+// Splice  Method --->Destructive method(mutates the original array)
+// Allows us to delete the elements
+// Allows us to add new elements
+//     The second arguement should always be 0
+// Allows us to delete elements
+const days = ["Sun", "Mon", "Tue", "Wed", "Thur", " Fri", "Sat"];
+console.log(days);
+const deletedDays = days.splice(3, 3);
+console.log(deletedDays);
+console.log(days);
+
+console.log(students);
+
+const months = ["Jan", "Feb", "March"];
+console.log(months);
+months.splice(3, 0, "April", "May", "June");
+console.log(months);
+console.log(months.length);
+
+console.log(months.splice(2, 2, "March16th"));
+console.log(months);
