c9b5f89d56629008bc95b61e0f1207d171a2ccbe
vincentmachariakimani <vincentmachariakimani@gmail.com>
2025-03-13 11:44:52 +0300

    aaray iteration

--- scripts/arrays.js
+++ scripts/arrays.js
@@ -1,100 +1,80 @@
+/**
+ * Array iteration --> running a statement once for each element
+ *
+ */
+let count = 0;
+const days = ["Mon", "Tue", "Wed", "Thur", "Fri"];
+console.log(days.length);
+console.log(days[2]);
+console.log(days[count]);
+
+while (count < days.length) {
+  console.log(count);
+  console.log(days[count]);
+  count++;
+}
+
+console.log(count);
+console.log("  ");
+
+//  For loop
+for (let count = 0; count < days.length; count++) {
+  console.log(count);
+  console.log(days[count]);
+}
+console.log(" ");
 const user = {
-  name: "Vincnent Kimani",
-  address: {
-    county: "Kiambu",
-    subCounty: "Juja",
-  },
+  age: 30,
+  lastName: "Kimani",
 };
-console.log(user.address.county);
-console.log(user[`address`][`subCounty`]);
 
-user[`age`] = 19;
-console.log(user);
+for (const key in user) console.log(user[key]);
+console.log(" ");
+// this one can not give  the index of the array
+for (const day of days) {
+  console.log(day);
+}
+console.log(" ");
 
-const students = [
-  `Paul`,
-  `Anna`,
-  `Sharon`,
-  `James`,
-  [`red`, `green`, { colorName: `blue` }],
-];
-console.log(students);
+// 1. Index(of)-->finds the position(index) of a given element
+//       or returns -1 if not found
+const positionOfThur = days.indexOf("Thur");
+console.log(positionOfThur);
+console.log(" ");
 
-console.log(students[4][2][`colorName`]);
-students[4] = "Nelson";
-students.splice(2, 0, "Bildad", "Denise", "George");
-// students.splice(2, 1);
+// 2.find() --> returns the first element is an array that meets a given conditon
+const day = days.find(function (day, index) {
+  if (index == 2) {
+    return day;
+  }
+});
+console.log(day);
+console.log(" ");
 
-// Arrray methods
-// --->Mutating array methods
-//----(Push, Shift, Pop, Unshift)----
+const scores = [34, 59, 63, 87, 54];
+const scoreAbove60 = scores.find((score) => {
+  if (score > 60) {
+    return score;
+  }
+});
+console.log(scoreAbove60);
+console.log(" ");
 
-const colors = ["Red", "Green", "Blue"];
-/**
- * Adding elements
- */
-// 1. Push method ---> elements at the end of an array
-console.log(colors);
-colors.push(`Green`, `yellow`);
-console.log(colors);
+function filterScoreAbove80(score) {
+  if (score > 80) {
+    return score;
+  }
+}
+const scoreAbove80 = scores.find(filterScoreAbove80);
+console.log(scoreAbove80);
 
-// 2. .unshift --. one or to elements at the beginning of an array
-colors.unshift(`White`, `Purple`);
-console.log(colors);
-/**
- * Removing elements
- */
+console.log(" ");
 
-// 1. .pop---> Removes the last element at the end of an array
-colors.pop();
-console.log(colors);
-
-const deletedColor = colors.pop();
-console.log(deletedColor);
-// .shift --> Removes elements at the begining of an array
-colors.shift();
-console.log(colors);
-const deletedColor2 = colors.shift();
-console.log(deletedColor2);
-
-/**
- * Slice and splice
- */
-// Slice --non-destructive method (Does not mutate the original array)
-// Allows us to make a copy of an array
-// Delete elements in an array
-
-const copyOfColors = colors.slice();
-console.log(copyOfColors);
-// Spread Operator(...)-> Makes a copy too
-const secondCopyOfColors = [...copyOfColors];
-
-const deletedColor3 = colors.slice(0);
-console.log(deletedColor);
-
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const deletedNumbers = numbers.slice(-5, -2);
-console.log(deletedNumbers);
-console.log(numbers);
-
-// Splice  Method --->Destructive method(mutates the original array)
-// Allows us to delete the elements
-// Allows us to add new elements
-//     The second arguement should always be 0
-// Allows us to delete elements
-const days = ["Sun", "Mon", "Tue", "Wed", "Thur", " Fri", "Sat"];
-console.log(days);
-const deletedDays = days.splice(3, 3);
-console.log(deletedDays);
-console.log(days);
-
-console.log(students);
-
-const months = ["Jan", "Feb", "March"];
-console.log(months);
-months.splice(3, 0, "April", "May", "June");
-console.log(months);
-console.log(months.length);
-
-console.log(months.splice(2, 2, "March16th"));
-console.log(months);
+// 3. filter() --> find and returns a list of elements that meet a given condition
+const scoresAbove60 = scores.filter((score) => {
+  if (score > 60) {
+    return score;
+  }
+});
+console.log(scores)
+console.log(scoresAbove60);
